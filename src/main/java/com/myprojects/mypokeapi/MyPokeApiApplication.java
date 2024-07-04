759a41a5c70ede2ccd005af109484687fbd20da0
AnagabrielaJimenez <ajimenezl@unsa.edu.pe>
2024-07-04 08:50:45 -0500

    chore: project initialization

---
+++ src/main/java/com/myprojects/mypokeapi/MyPokeApiApplication.java
@@ -0,0 +1,13 @@
+package com.myprojects.mypokeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPokeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPokeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojects/mypokeapi/MyPokeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.mypokeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPokeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
